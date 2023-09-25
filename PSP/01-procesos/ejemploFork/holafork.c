dc2fe8bc0e85c6e217d9cde8f731e14b379e5a8f
XUEXIAO <zhouyichen1998@gmail.com>
2023-09-25 21:30:28 +0200

    PSP

---
+++ PSP/01-procesos/ejemploFork/holafork.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void)
+{
+    pid_t id_hijo;
+
+    int n = 42;
+    double pi = 3.14;
+
+    //¡¡¡Clonacion!!!
+    id_hijo = fork();
+
+    //Hay dos procesos
+    if(id != 0){
+        //¿Padre?
+        printf("Soy el padre %d\n",id_hijo);
+    }else{
+        //¿?
+        printf("Soy el hijo %d\n",id_hijo);
+    }
+    
+
+
+
+    return 0;
+}
