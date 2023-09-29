31f754f3fe284b4aea100b8418090184ddbfb5e0
XUEXIAO <zhouyichen1998@gmail.com>
2023-09-29 16:06:09 +0200

    Subir AD cole

--- AD/UNIT1/Ejercicios/ej1_1_2.java
+++ AD/UNIT1/Ejercicios/ej1_1_2.java
@@ -1,19 +1,31 @@
-import java.io.File;
+import java.io.*;
 import java.util.Arrays;
 
 public class ej1_1_2 {
-    public static void main(String[] args) {
+    public static final String FILESEPARATOR = File.separator;
 
-        if (args.length==1){
-            File f = new File(args[0]);
-            System.out.println(f);
-        }
-        File f = new File("ewq");
-        System.out.println(Arrays.toString(f.list()));
-        if(f.exists()){
-            System.out.println("si exist.");
+    public static void main(String[] args) {
+        if (args.length!=1){
+            System.out.println("Introducir una ruta de directorio por args(Comando).");
+
         }else{
-            System.out.println("No exist.");
+            //obtener la ruta con separator de ordenador 
+            String archivoRuta = "."+FILESEPARATOR+args[0];
+
+            File f = new File(archivoRuta);
+            
+            if(f.isDirectory()){
+                System.out.println("Directorio si existe.");
+                //imprimer los archivos
+                File[] listaArchivos = f.listFiles();
+                for (File file : listaArchivos) {
+                    System.out.println(file);
+                }
+            }else{
+                System.out.println("Directorio no existe.");
+            }
         }
+       
+
     }
 }
