71c74a7bd3ee7889c7b63ea3222538ce332e7a88
at4DAM2 <at4DAM2@informatica.iesjc>
2023-11-20 19:22:08 +0100

    Subir preguntas para examen de Speaking

---
+++ PSP/01-procesos/EjerciciosC/05-entrenamiento/Entrenamiento4.c
@@ -0,0 +1,2 @@
+//Crea un programa que reciba un número n por parámetro. El programa creará n hijos y les enviará una señal a cada uno de ellos para matarlos.
+#include<stdio.h>
---
+++ PSP/02-hilos/EjemploThread/PrincipalConWait.java
@@ -0,0 +1,39 @@
+public class PrincipalConWait {
+
+    private static final int TOTAL_CARRERA = 100;
+    private static final int NUM_CORREDORES = 10;
+    private static final int STAR_DORSAL = 1000;
+
+    public static void main(String[] args) {
+        Thread corredores[] = new Thread[NUM_CORREDORES];
+
+        for (int i = 0; i < NUM_CORREDORES; i++) {
+            corredores[i] = new Thread(new CorredorConWait(TOTAL_CARRERA, STAR_DORSAL + i));
+        }
+
+        System.out.println("La carrera va a comenzar!!!");
+        for (int i = 0; i < NUM_CORREDORES; i++) {
+            corredores[i].start();
+            try {
+                synchronized (corredores[i]) {
+                    corredores[i].wait();
+                }
+
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        for (int i = 0; i < NUM_CORREDORES; i++) {
+            try {
+                corredores[i].join();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        corredores[0].notifyAll();
+
+        System.out.println("¡La carrera ha terminado!");
+    }
+}
