682179a484d801bbb881d20883cb5f9af6661763
BIG BOSS <zhouyichen1998@gmail.com>
2024-01-10 12:54:19 +0100

    ejercicios de AD

--- PSP/01-procesos/EjerciciosC/01-ejercicios/NumeroPrimo.c
+++ PSP/01-procesos/EjerciciosC/01-ejercicios/NumeroPrimo.c
@@ -2,6 +2,7 @@
 #include<stdbool.h>
 int main(void)
 {   
+    
     bool primo = true;
     int num;
     printf("ingresar un número entero positivo y determine si es un número primo o no.\n");
@@ -21,4 +22,17 @@ int main(void)
     }
     
     return 0;
+    /*
+    int number;
+ 
+    printf("请输入一个整数: ");
+    scanf("%d", &number);
+ 
+    // 判断这个数最后一位是1这为奇数
+    if(number&1)
+        printf("%d 是奇数。", number);
+    else
+        printf("%d 是偶数。", number);
+ 
+    return 0;*/
 }
--- PSP/01-procesos/EjerciciosC/01-ejercicios/TablaMultiplicar1.c
+++ PSP/01-procesos/EjerciciosC/01-ejercicios/TablaMultiplicar1.c
@@ -3,7 +3,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #define N 10
-
 void imprimirTabla(int num)
 {
     for (int i = 1; i <= num; i++)
@@ -18,24 +17,31 @@ int main(int argc, char const *argv[])
     pid_t child_pids[N];
     pid_t idHijo;
     for (int i = 1; i < N; i++)
-    {
+    {           
         sleep(2);
         child_pids[i] = idHijo = fork();
         if (idHijo == 0)
         {
             imprimirTabla(i);
-            exit(42 + i);
-        }
-        else
-        {
-            int status;
-            waitpid(child_pids[i], &status, 0);
-            if (WIFEXITED(status))
-            {
-                printf("Padre - hijo %d terminado con estado %d\n", child_pids[i], WEXITSTATUS(status));
-            }
+            exit(42+i);
         }
     }
-
+    /*
+    for (int j = 0; j < N; j++)
+    {
+        printf("%d - ",j);
+        int status;
+        wait(&status); 
+        printf("%d ; ",WEXITSTATUS(status));
+    }*/
+    for (int j = 0; j < N; j++)
+    {
+        printf("%d - ",child_pids[j]);
+        int status;
+        waitpid(child_pids[j],&status,0); 
+        if(WIFEXITED(status)){
+            printf("%d ; ",WEXITSTATUS(status));
+        }
+    }
     return 0;
 }
