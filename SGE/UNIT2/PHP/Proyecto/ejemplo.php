eafe6d21328f9bae36f67a51b2b1a01b5d047dbd
BIG BOSS <zhouyichen1998@gmail.com>
2024-02-06 15:01:36 +0100

    sge proy

--- SGE/UNIT2/PHP/Proyecto/ejemplo.php
+++ SGE/UNIT2/PHP/Proyecto/ejemplo.php
@@ -1,47 +1,152 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Mi Sitio Web</title>
-  <style>
-/* Estilos para el navbar */
-.navbar {
-    background-color: #333;
-    color: white;
-    padding: 10px;
-    text-align: center;
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Sitio Web</title>
+    <link href="http://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        div.search {
+            padding: 30px 0;
+        }
+
+        form {
+            position: relative;
+            width: 300px;
+            margin: 0 auto;
+        }
+
+        input,
+        button {
+            border: none;
+            outline: none;
+        }
+
+        input {
+            width: 100%;
+            height: 42px;
+            padding-left: 13px;
+        }
+
+        button {
+            height: 42px;
+            width: 42px;
+            cursor: pointer;
+            position: absolute;
+        }
+
+        /*搜索框1*/
+        .bar1 {
+            background: #A3D0C3;
+        }
+
+        .bar1 input {
+            border: 2px solid #7BA7AB;
+            border-radius: 5px;
+            background: #F9F0DA;
+            color: #9E9C9C;
+        }
+
+        .bar1 button {
+            top: 0;
+            right: 0;
+            background: #7BA7AB;
+            border-radius: 0 5px 5px 0;
+        }
+
+        .bar1 button:before {
+            content: "f002";
+            font-family: FontAwesome;
+            font-size: 16px;
+            color: #F9F0DA;
+        }
+        table {
+	border-collapse: collapse;
+	margin: 0 auto;
+	text-align: center;
 }
 
-/* Estilos para el div */
-.mi-div {
-    margin-top: 50px; /* Ajusta el valor según tus necesidades */
-    padding: 20px;
-    background-color: #f0f0f0;
-    text-align: center;
+/*ESTILOS PARA TABLA*/
+table td,
+table th {
+	border: 1px solid #cad9ea;
+	color: #666;
+	height: 30px;
 }
 
-  
-  </style>
+table thead th {
+	background-color: #CCE8EB;
+	width: 100px;
+}
+
+table tr:nth-child(odd) {
+	background: #fff;
+}
+
+table tr:nth-child(even) {
+	background: #F5FAFA;
+}
+    </style>
 </head>
+
 <body>
 
-<div class="navbar">
-    <p>hola</p>
-  <a href="#inicio">Inicio</a>
-  <a href="#acerca-de">Acerca de</a>
-  <a href="#servicios">Servicios</a>
-  <a href="#contacto">Contacto</a>
-</div>
-<div class="mi-div">
-        <!-- Contenido del div -->			<h1>Consulta de libros</h1>
-			<h1>Consulta de libros</h1>
-
-<h1>Consulta de libros</h1>
-<h1>Consulta de libros</h1>
-<h1>Consulta de libros</h1>
+    <div class="navbar">
+        <p>hola</p>
+        <a href="#inicio">Inicio</a>
+        <a href="#acerca-de">Acerca de</a>
+        <a href="#servicios">Servicios</a>
+        <a href="#contacto">Contacto</a>
     </div>
-<!-- El resto del contenido de tu página -->
+    <div class="mi-div">
+        <!-- Contenido del div -->
+        <h1>Consulta de libros</h1>
+        <h1>Consulta de libros</h1>
 
-</body>
+        <h1>Consulta de libros</h1>
+        <h1>Consulta de libros</h1>
+        <h1>Consulta de libros</h1>
+    </div>
+    <!-- El resto del contenido de tu página -->
+    <div id="container">
+        <div class="search bar1">
+            <h1>Consulta de libros</h1>
+            <form>
+                <input type="text" placeholder="请输入您要搜索的内容...">
+                <button type="submit"></button>
+            </form>
+            <div class="cuerpo">
+                <div class="titulo">
+                    <table width="90%" class="table">
+                        <caption>
+
+                        </caption>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Titulo</th>
+                                <th>Autor</th>
+                                <th>Isbn</th>
+                                <th>Estado</th>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <td>1</td>
+                            <td>COLMENA</td>
+                            <td>LOPEZ DE VEGA</td>
+                            <td>22132133</td>
+                            <td>Prestado</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
 </html>
--- SGE/UNIT2/PHP/Proyecto/home.php
+++ SGE/UNIT2/PHP/Proyecto/home.php
@@ -1,47 +1,82 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Bootstrap Example</title>
-    <link rel="stylesheet" href="./headers.css" type="text/css">
-  </head>
-  <body class="p-3 m-0 border-0 bd-example m-0 border-0">
-    <div class="header">
-				<div class="Bienvenido">
-          <img class="Favicon32x32" src="#" alt="Favicon32x32">
-          <p> Bienvenido</p>
-				</div>
-				<div class="Nav-Menu">
-					<div class="Dest">
-						<p>Tiempo</p>
-					</div>
-					<nav class="Menu">
-						<ul>
-							<li>
-								<a href="#">Idioma</a>
-									<ul>
-										<li><a href="#">ES</a></li>
-										<li><a href="#">IN</a></li>
-									</ul>
-							</li>
-							<li>
-								<a href="#">Mis datos</a>
-							</li>
-							<li>
-								<a href="#">Logout</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Bootstrap Example</title>
+	<link rel="stylesheet" href="./headers.css" type="text/css">
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+</head>
+
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+	<div class="header">
+		<div class="Bienvenido">
+			<img class="Favicon32x32" src="#" alt="Favicon32x32">
+			<p> Bienvenido</p>
+		</div>
+		<div class="Nav-Menu">
+			<div class="Dest">
+				<p>Tiempo</p>
 			</div>
+			<nav class="Menu">
+				<ul>
+					<li>
+						<a href="#">Idioma</a>
+						<ul>
+							<li><a href="#">ES</a></li>
+							<li><a href="#">IN</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Mis datos</a>
+					</li>
+					<li>
+						<a href="#">Logout</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
 
-    <div class="cuerpo">
-      <div class="titulo">
-        <h1>Consulta de libros</h1>
+	<div class="cuerpo">
+		<div class="search bar1">
+				<h1>Consulta de libros</h1>
+				<form >
+					<input type="text" placeholder="Introducir el titulo de libro...">
+					<button type="submit" ><img src="./fi-rr-search.svg" width="25" height="25" alt="Icono"></button>
+				</form>
 
-      </div>
-    </div>
-  </body>
+					<div class="titulo">
+						<table width="90%" class="table">
+							<caption>
+
+							</caption>
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Titulo</th>
+									<th>Autor</th>
+									<th>Isbn</th>
+									<th>Estado</th>
+								</tr>
+							</thead>
+							<tr>
+								<td>1</td>
+								<td>COLMENA</td>
+								<td>LOPEZ DE VEGA</td>
+								<td>22132133</td>
+								<td>Prestado</td>
+							</tr>
+						</table>
+					</div>
+				<form >
+					<input type="text" placeholder="Introducir el titulo de libro...">
+					<button type="submit" ><img src="./fi-rr-search.svg" width="25" height="25" alt="Icono"></button>
+				</form>
+			</div>
+	</div>
+</body>
+
 </html>
